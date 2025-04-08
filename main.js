c6e3d873e03a7183ebd3e5e0095f848f855a64ee
yessamin <136747651+yessaminandrade@users.noreply.github.com>
2025-04-08 10:15:21 -0600

    Se agregan los archivos de inicio del proyecto

---
+++ main.js
@@ -0,0 +1,48 @@
+/// console.log(document.getElementById("encabezado1")); //Estamos accediendo a la etiqueta h1 con el id "encabezado1".
+
+// let encabezado1 = document.getElementById("encabezado1");
+// encabezado1.innerText = "Ejercicio DOM";
+// console.log(encabezado1.innerText);
+
+//DOM Exercise
+
+// let encabezado2 = document.getElementById("encabezado2");
+// encabezado2.innerText = "DOM Exercise";
+// console.log(encabezado2.innerText);
+
+let btnMostrar = document.getElementById("btnMostrar");
+let encabezado1 = document.getElementById("encabezado1");
+let encabezado2 = document.getElementById("encabezado2");
+let listas = document.getElementsByTagName("ul");
+
+let elementos = document.getElementsByClassName("list-group-item");
+
+let otroElemento = document.querySelector("ul>li"); //primer elemento
+
+let otrosElementos = document.querySelectorAll("ul>li"); //todos
+
+console.log(otrosElementos.length);
+
+console.log("otroElemento: ", otroElemento);
+
+console.log(listas.length); //2
+// console.log(listas[0]);
+console.log(listas.item(1));
+
+console.log(elementos.item(2));
+
+console.log(elementos.length); //10
+
+let contador = 0; 
+function modifica(){
+    // encabezado1.innerText = "<em>Ejercicio</em> DOM"
+    encabezado1.innerHTML += "<em>Ejercicio</em> DOM";
+    encabezado2.innerText = ++contador;   
+} //modifica
+
+//console.log(encabezado1.innerText);
+
+btnMostrar.addEventListener("click", function (event){
+    event.preventDefault(); //no hagas lo que haces por defecto
+    console.log("botón btnMostrar presionado");
+});
